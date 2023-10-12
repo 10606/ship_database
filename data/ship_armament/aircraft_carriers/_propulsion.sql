a6fb5bd2b1f472861be1b2f3e962f67f2ce184f6
10606 <gav_sbor@mail.ru>
2023-10-12 22:14:35 +0300

    propulsion for Akagi and Kaga

---
+++ data/ship_armament/aircraft_carriers/_propulsion.sql
@@ -0,0 +1,99 @@
+
+/* турбины */
+insert into steam_turbine
+    (id, name_ru, name_en, in_service, rpm, power, stages)
+    values
+    (46, 'Гидзицу-Хонбу высокого давления',             'Gijitsu-Honbu high pressure',          null,   null,   null, null), /* 8* Акаги до 1938 */
+    (47, 'Гидзицу-Хонбу среднего давления',             'Gijitsu-Honbu intermediate pressure',  null,   null,   null, null), /* 4* Акаги до 1938 */
+    (48, 'Гидзицу-Хонбу низкого давления',              'Gijitsu-Honbu low pressure',           null,   null,   null, null), /* 8* Акаги до 1938 */
+    
+    (49, 'Кампон высокого давления',                    'Kampon high pressure',                 null,   null,   null, null), /* 8* Акаги после 1938 */
+    (50, 'Кампон среднего давления',                    'Kampon intermediate pressure',         null,   null,   null, null), /* 4* Акаги после 1938 */
+    (51, 'Кампон низкого давления',                     'Kampon low pressure',                  null,   null,   null, null), /* 4* Акаги после 1938 */
+    
+    (52, 'Браун-Кертис высокого давления',              'Brown-Curtis high pressure',           null,   null,   null, null), /* 4* Кага до 1934 */
+    (53, 'Браун-Кертис среднего давления',              'Brown-Curtis intermediate pressure',   null,   null,   null, null), /* 4* Кага до 1934 */
+    (54, 'Браун-Кертис низкого давления',               'Brown-Curtis low pressure',            null,   null,   null, null), /* 8* Кага до 1934 */
+    
+    (55, 'Кампон высокого давления',                    'Kampon high pressure',                 null,   null,   null, null), /* 8* Кага после 1936 */
+    (56, 'Кампон среднего давления',                    'Kampon intermediate pressure',         null,   null,   null, null), /* 4* Кага после 1936 */
+    (57, 'Кампон низкого давления',                     'Kampon low pressure',                  null,   null,   null, null); /* 4* Кага после 1936 */
+    
+    
+    
+/* котлы */
+insert into boiling_types
+    (id, name_ru, name_en, value, temperature, pressure, heating_surface)
+    values
+    (18, 'Кампон Ро Го',            'Kampon Ro Go large',   2,  270,    19.5,   null), /* 11* Акаги до 1938 */
+    (19, 'Кампон Ро Го малый',      'Kampon Ro Go small',   1,  270,    19.5,   null), /*  8* Акаги до 1938 */
+    
+    (20, 'Кампон Ро Го',            'Kampon Ro Go large',   2,  310,    23,     null), /* 11* Акаги после 1938 */
+    (21, 'Кампон Ро Го малый',      'Kampon Ro Go small',   2,  310,    23,     null), /*  8* Акаги после 1938 */
+    
+    (22, 'Кампон Ро Го',            'Kampon Ro Go large',   2,  270,    19.5,   null), /*  8* Кага до 1934 */
+    (23, 'Кампон Ро Го малый',      'Kampon Ro Go small',   1,  270,    19.5,   null), /*  4* Кага до 1934 */
+
+    (24, 'Кампон Ро Го',            'Kampon Ro Go large',   2,  310,    23,     null); /*  8* Кага после 1936 */
+    
+    
+    
+/* силовая установка */
+insert into external_burn_list
+    (id, mass, max_power, in_service)
+    values
+    (15,  null, 131000,  null), /* Акаги до 1938 */
+    (16,  null, 133000,  null), /* Акаги после 1938 */
+    (17,  null,  91000,  null), /* Кага до 1934 */
+    (18,  null, 127400,  null); /* Кага после 1936 */
+    
+insert into external_burn_boiling
+    (object_id, item_id, count)
+    values
+    /* Акаги */
+    (15, 18, 11),
+    (15, 19,  8),
+    
+    (16, 20, 11),
+    (16, 21,  8),
+    
+    /* Кага */
+    (17, 22,  8),
+    (17, 23,  4),
+    
+    (18, 24,  8);
+
+insert into external_burn_machines
+    (object_id, item_id, count)
+    values
+    /* Акаги */
+    (15, 46, 8),
+    (15, 47, 4),
+    (15, 48, 8),
+
+    (16, 49, 8),
+    (16, 50, 4),
+    (16, 51, 4),
+    
+    /* Кага */
+    (17, 52, 4),
+    (17, 53, 4),
+    (17, 54, 8),
+    
+    (18, 55, 8),
+    (18, 56, 4),
+    (18, 57, 4);
+  
+    
+    
+insert into ship_propulsion
+    (ship_id, propulsion_id, amount, date_from, date_to)
+    values
+    /* Акаги */
+    (57, 15, 1,  '1927-03-27', '1935-10-24'),
+    (57, 16, 1,  '1938-08-31', '1942-06-05'),
+    
+    /* Кага */
+    (58, 17, 1,  '1928-03-31', '1934-06-25'),
+    (58, 18, 1,  '1936-06-10', '1942-06-04');
+
--- tables/propulsion.sql
+++ tables/propulsion.sql
@@ -9,6 +9,24 @@ create table propulsion
     primary key (id)
 );
 
+/* emulate unique key */
+create or replace function propulsion_unique_key ()
+returns trigger
+    language plpgsql
+as $$
+declare
+begin
+    set transaction isolation level read committed;
+    if (exists (select * from propulsion where (propulsion.id = new.id)))
+    then
+        raise exception 'ограничение уникального ключа propulsion.id';
+    else
+    end if;
+    return new;
+end;
+$$;
+
+
 create table diesel_list
 (
     name_ru varchar(100),
@@ -27,6 +45,13 @@ alter table diesel_list add check ((tact_value = 2) or (tact_value = 4));
 alter table diesel_list add check (cilinder_diameter * cilinder_diameter * pi() / 4 * cilinder_stroke * cilinder_count * 1.2 >= volume_of_engine * 100 * 100 * 100);
 alter table diesel_list add check (cilinder_diameter * cilinder_diameter * pi() / 4 * cilinder_stroke * cilinder_count <= 1.2 * volume_of_engine * 100 * 100 * 100);
 
+drop trigger if exists diesel_list_unique_key_trigger_em on diesel_list;
+create trigger diesel_list_unique_key_trigger_em
+    before insert
+    on diesel_list
+    for each row
+    execute procedure propulsion_unique_key();
+
 
 create table boiling_types
 (
@@ -44,6 +69,7 @@ alter table boiling_types add check ((value >= 0) and (value <= 3));
 alter table boiling_types add check (temperature > 0);
 alter table boiling_types add check (pressure > 0);
 
+
 create table machine_types
 (
     id int not null,
@@ -53,6 +79,24 @@ create table machine_types
     
     primary key (id)
 );
+
+/* emulate unique key */
+create or replace function machine_types_unique_key ()
+returns trigger
+    language plpgsql
+as $$
+declare
+begin
+    set transaction isolation level read committed;
+    if (exists (select * from machine_types where (machine_types.id = new.id)))
+    then
+        raise exception 'ограничение уникального ключа machine_types.id';
+    else
+    end if;
+    return new;
+end;
+$$;
+
 
 create table steam_turbine
 (
@@ -64,21 +108,70 @@ create table steam_turbine
 ) inherits(machine_types);
 alter table steam_turbine add check (stages > 0);
 
+drop trigger if exists steam_turbine_unique_key_trigger_em on steam_turbine;
+create trigger steam_turbine_unique_key_trigger_em
+    before insert
+    on steam_turbine
+    for each row
+    execute procedure machine_types_unique_key();
+
+/* emulate unique key */
+create or replace function steam_turbine_unique_key ()
+returns trigger
+    language plpgsql
+as $$
+declare
+begin
+    set transaction isolation level read committed;
+    if (exists (select * from steam_turbine where (steam_turbine.id = new.id)))
+    then
+        raise exception 'ограничение уникального ключа steam_turbine.id';
+    else
+    end if;
+    return new;
+end;
+$$;
+
+
 create table steam_turbine_reverse
 (
     primary key (id)
 ) inherits(steam_turbine);
 
+drop trigger if exists steam_turbine_reverse_unique_key_trigger_em on steam_turbine_reverse;
+create trigger steam_turbine_reverse_unique_key_trigger_em
+    before insert
+    on steam_turbine_reverse
+    for each row
+    execute procedure steam_turbine_unique_key();
+
+    
 create table steam_turbine_cruise
 (
     primary key (id)
 ) inherits(steam_turbine);
 
+drop trigger if exists steam_turbine_cruise_unique_key_trigger_em on steam_turbine_cruise;
+create trigger steam_turbine_cruise_unique_key_trigger_em
+    before insert
+    on steam_turbine_cruise
+    for each row
+    execute procedure steam_turbine_unique_key();
+
+    
 create table steam_machine
 (
     primary key (id)
 ) inherits(machine_types);
 
+drop trigger if exists steam_machine_unique_key_trigger_em on steam_machine;
+create trigger steam_machine_unique_key_trigger_em
+    before insert
+    on steam_machine
+    for each row
+    execute procedure machine_types_unique_key();
+
+    
 create table steam_machine_cilinders
 (
     machine_id          int not null,
@@ -93,11 +186,20 @@ alter table steam_machine_cilinders add check (cilinder_diameter > 0);
 alter table steam_machine_cilinders add check (cilinder_stroke > 0);
 alter table steam_machine_cilinders add check (cilinder_count > 0);
 
+
 create table external_burn_list
 (
     primary key (id)
 ) inherits(propulsion);
 
+drop trigger if exists external_burn_list_unique_key_trigger_em on external_burn_list;
+create trigger external_burn_list_unique_key_trigger_em
+    before insert
+    on external_burn_list
+    for each row
+    execute procedure propulsion_unique_key();
+
+    
 create table external_burn_boiling
 (
     object_id   int not null,
@@ -111,6 +213,7 @@ create table external_burn_boiling
         on delete restrict on update cascade
 );
 alter table external_burn_boiling add check (count > 0);
+
 
 create table external_burn_machines
 (
